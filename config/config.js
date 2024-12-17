a79a8a7728b69f72a5e70fb2724da22fe15ba02f
datthanh123zz <ltdata23125@cusc.ctu.edu.vn>
2024-12-18 01:28:27 +0700

    first commit

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/library');
+        await mongoose.connect('mongodb+srv://dat:thanhdat@thanhdat.lgnoy.mongodb.net/?retryWrites=true&w=majority&appName=thanhdat');
         console.log('Kết nối MongoDB thành công');
     } catch (err) {
         console.error('Lỗi kết nối MongoDB:', err);
